7231b3ed581673f3634ed8c67a3e1579276db415
prokxy <casperraf.game@gmail.com>
2022-12-21 14:26:21 +0100

    Added deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# Making bspwmrc executable
+chmod +x bspwmrc
+
+# create backup old dotfiles 
+echo "Backing up dotfiles.."
+mkdir -p old_dot_files
+sudo mv ~/.config/bspwm/bspwmrc ./old_dot_files/
+sudo mv ~/.config/sxhkd/sxhkdrc ./old_dot_files/
+
+# Insert new dotfiles
+echo "Inserting new dotfiles.."
+sudo cp bspwmrc ~/.config/bspwm/
+sudo cp sxhkdrc ~/.config/sxhkd/
+
+echo "All done!"
 
