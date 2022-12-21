21b8eb0aebe02326929c2dcd33c86065adc920f9
prokxy <casperraf.game@gmail.com>
2022-12-21 14:40:41 +0100

    hello

---
+++ collect.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Collect dotfiles
+echo "Copying your dotfiles"
+sudo cp ~/.config/bspwm/bspwmrc .
+sudo cp ~/.config/sxhkd/sxhkdrc .
+
+
+
+echo "All done!"
--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,9 @@ sudo mv ~/.config/sxhkd/sxhkdrc ./old_dot_files/
 
 # Insert new dotfiles
 echo "Inserting new dotfiles.."
+sudo chmod 755 bspwmrc
 sudo cp bspwmrc ~/.config/bspwm/
+sudo chmod 755 sxhkdrc
 sudo cp sxhkdrc ~/.config/sxhkd/
 
 echo "All done!"
