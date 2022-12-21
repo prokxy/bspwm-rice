a58f00d81cb0ae646307a9ef0cb60d63ca64650e
prokxy <99291552+prokxy@users.noreply.github.com>
2022-12-21 20:16:33 +0000

    added kitty

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,14 @@ mkdir -p old_dot_files
 sudo mv ~/.config/bspwm ./old_dot_files/
 sudo mv ~/.config/sxhkd ./old_dot_files/
 sudo mv ~/.config/picom ./old_dot_files/
+sudo mv ~/.config/kitty ./old_dot_files/
 
 # Insert new dotfiles
 echo "Inserting new dotfiles.."
 cp -r bspwm ~/.config/
 cp -r sxhkd ~/.config/
 cp -r picom ~/.config/
+cp -r kitty ~/.config/
 
 echo "All done!"
 
